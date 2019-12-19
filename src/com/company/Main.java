cba0932d82cc5692da8b97db2d958e0295dd6d89
Anca <ancamicle@hotmail.com>
2019-12-19 20:47:33 +0200

    ok

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+        byte x = 12;
+        short y = x++;
+        float z = x+y;
+        String mystring = "result sum = y+z";
+
+        System.out.println( z);
+        System.out.println(mystring);
+
+
+
+
+
+    }
+}
